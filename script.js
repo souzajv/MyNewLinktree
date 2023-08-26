a149ab52bd8165bb0f36bf16f6d318d758beecfc
João Souza <joovictorsouza5@gmail.com>
2023-08-26 00:26:19 -0300

    finalização do css e do light mode

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 function toggleMode() {
     const hmtl = document.documentElement
- 
-    if(hmtl.classList.contains('light')) {
-        hmtl.classList.remove('light') 
-    } else {
-    html.classList.add('light')
-    }
+    hmtl.classList.toggle("light")
+    
 }
